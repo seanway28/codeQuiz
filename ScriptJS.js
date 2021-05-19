2b00e8411d4c9f54b43dbd48e0e97bfd834ba200
seanway28 <seanway28@gmail.com>
2021-05-19 15:07:20 -0400

    API attmpt json

--- ScriptJS.js
+++ ScriptJS.js
@@ -1,1 +1,14 @@
-codeQuiz
+// Create timer
+
+// Event handler for Start button (starts timer) and fetches first question
+
+const questions = getData(`https://quizapi.io/api/v1/questions?apiKey=TNcx4VpjC6VDN3gD5wRGbwOXe8hWHUJFBYZ5EjCV`, (data) => data.json());
+
+function getData(url, cb) {
+    fetch(url)
+      .then(response => response.json())
+      .then(result => cb(result));
+  }
+
+console.log(questions)
+// Append first question to DOM
